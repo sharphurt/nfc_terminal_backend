31ea67931812287f35a400138b48068f5703e026
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-11-07 22:47:28 +0500

    Project created - initial commit

---
+++ src/main/java/ru/catstack/nfc_terminal/NfcTerminalApplication.java
@@ -0,0 +1,13 @@
+package ru.catstack.nfc_terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfcTerminalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfcTerminalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/catstack/nfc_terminal/NfcTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.catstack.nfc_terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfcTerminalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
